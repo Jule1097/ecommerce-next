898397b6704eec5d4df46aa551b1dee9eb641fc6
Jule1097 <julianibarra1797@gmail.com>
2022-08-22 00:12:48 -0300

    added keys to tr

--- components/Carrito.js
+++ components/Carrito.js
@@ -63,14 +63,14 @@ const Carrito = ( columns) => {
         <table>
           <thead>
             <tr>
-              {setColumns.map((title) => (
+              {setColumns.map(title => (
                 <th>{title.headerName}</th>
               ))}
             </tr>
           </thead>
           <tbody>
-          {products.map((e, index) => 
-            <tr key={index}>
+          {products.map(e => 
+            <tr>
               {setColumns.map(header => (<td>{printColumnsField(e, header.field)}</td>))}
             </tr>)}
           </tbody>
